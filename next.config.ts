9e2f2404b8bab771d2c3302292622d7ca6246ebd
Andrii Halchyshak <agalchisak@gmail.com>
2026-05-05 13:44:31 +0300

    fix: transpile FullCalendar packages to resolve export syntax error on production

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: [
+    "@fullcalendar/common",
+    "@fullcalendar/daygrid",
+    "@fullcalendar/interaction",
+    "@fullcalendar/react",
+    "@fullcalendar/timegrid",
+  ],
 };
 
 export default nextConfig;
